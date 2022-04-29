b263837f8a8b34b6e866bf4940fb6a6a9118f41d
James Williams <cfossguy@gmail.com>
2022-04-29 19:44:46 -0400

    upgrade to 6.0

--- dotnet-core/Program.cs
+++ dotnet-core/Program.cs
@@ -1,49 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System.Diagnostics;
-using System.Net.Http;
-using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
 
-namespace dotnet_core
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+builder.Services.AddOpenTelemetryTracing(b =>
 {
-    public class Program
+    b.AddJaegerExporter(o =>
     {
-        public static void Main(string[] args)
-        {
-            var serviceName = "MyCompany.MyProduct.MyService";
-            var serviceVersion = "1.0.0";
-            var builder = WebApplication.CreateBuilder(args);
-            
-            // Configure important OpenTelemetry settings, the console exporter, and automatic instrumentation
-            builder.Services.AddOpenTelemetryTracing(b =>
-            {
-                b
-                    .AddConsoleExporter()
-                    .AddSource(serviceName)
-                    .SetResourceBuilder(
-                        ResourceBuilder.CreateDefault()
-                            .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
-                    .AddHttpClientInstrumentation()
-                    .AddAspNetCoreInstrumentation();
-            });
-            
-            var app = builder.Build();
-            var httpClient = new HttpClient();
-            app.Run();
-            //CreateHostBuilder(args).Build().Run();
-        }
+        o.AgentHost = "http://10.100.158.8";
+        o.AgentPort = 9411; // use port number here
+    });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseMetricServer();
+app.UseHttpMetrics();
+app.MapControllers();
+
+app.Run();
