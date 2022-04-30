a960781ddee44a160b9e0d670937e43c1435c812
James Williams <cfossguy@gmail.com>
2022-04-29 21:08:10 -0400

    upgrade to 6.0

--- dotnet-core/Program.cs
+++ dotnet-core/Program.cs
@@ -1,37 +1,28 @@
+using System.Net;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var serviceName = "MyCompany.MyProduct.MyService";
-var serviceVersion = "1.0.0";
-
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddOpenTelemetryTracing(b =>
-{
-    b.AddJaegerExporter(o =>
-    {
-        o.AgentHost = "http://10.100.158.8";
-        o.AgentPort = 9411; // use port number here
-    });
-});
-
 // Configure important OpenTelemetry settings, the console exporter, and automatic instrumentation
 builder.Services.AddOpenTelemetryTracing(b =>
 {
     b
-        .AddSource(serviceName)
-        .SetResourceBuilder(
-            ResourceBuilder.CreateDefault()
-                .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation();
+    .AddSource("dotnet-core")
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault().AddService("dotnet-core"))
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation()
+    .AddOtlpExporter(opt =>
+    {
+        opt.Endpoint = new Uri("10.100.158.8:4317");
+    });
 });
 
 var app = builder.Build();
