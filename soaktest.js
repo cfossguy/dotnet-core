62e00ee9cd692730fd0285983f7a79994ec37b3b
James Williams <cfossguy@gmail.com>
2022-05-04 20:27:27 -0400

    1.1.3pre

--- soaktest.js
+++ soaktest.js
@@ -12,9 +12,9 @@ export default function () {
     http.get('http://35.188.223.237/Match/slow/500');
     http.get('http://35.188.223.237/Match/slow/400');
     http.get('http://35.188.223.237/Match/slow/300');
-    http.get('http://35.188.223.237/Match/fast/50');
-    http.get('http://35.188.223.237/Match/fast/30');
     http.get('http://35.188.223.237/Match/fast/20');
     http.get('http://35.188.223.237/Match/fast/10');
+    http.get('http://35.188.223.237/Match/fast/5');
+    http.get('http://35.188.223.237/Match/fast/1');
     http.get('http://35.188.223.237/Match/roulette');
 }
